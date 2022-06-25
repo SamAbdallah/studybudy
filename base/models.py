3fbd473ba1fc5f58e8d10cdf57ce674deda6ad2e
SamAbdallah <saa081@student.bau.edu.lb>
2022-06-26 00:19:15 +0300

    creating the option of adding a message in a room and adding participants to database

--- base/models.py
+++ base/models.py
@@ -19,6 +19,7 @@ class Room(models.Model):
     host= models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     topic=models.ForeignKey(topic,on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=200)
+    participants=models.ManyToManyField(User,related_name='participant',blank=True)
     description=models.TextField(null=True,blank=True)
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from multiprocessing import context
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Room,topic
+from .models import Message, Room,topic
 from .forms import RoomForm
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
@@ -80,7 +80,19 @@ def home(request):
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
-    context={'room':room}        
+    room_messages=room.message_set.all().order_by('-created')
+    if request.method== 'POST' :
+     message=Message.objects.create(
+
+        user=request.user, #authenticated
+        room=room, 
+        body=request.POST.get('body')
+
+     )
+
+     return redirect ('room',pk=room.id)
+
+    context={'room':room,'room_messages':room_messages}        
 
     return render(request,'base/room.html',context)  
 
